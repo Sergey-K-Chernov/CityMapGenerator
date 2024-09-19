998d9b3220dcec298b63ebcaa662d6e0e6b7cc33
Sergey-K-Chernov <sergey.k.chernov@yandex.ru>
2024-09-19 03:16:16 +0300

    Refactoring

--- generator/generator.go
+++ generator/generator.go
@@ -14,23 +14,33 @@ func degToRad(value float64) float64 {
 	return value * math.Pi / 180.
 }
 
+type Point struct {
+	X float64
+	Y float64
+}
+
 func randFloat(min, max float64) float64 {
 	return min + (max-min)*rand.Float64()
 }
 
+func generateRadialRandomPoint(angle_min, angle_max, radius_min, raduis_max float64) Point {
+	angle := randFloat(angle_min, angle_max)
+	radius := randFloat(radius_min, raduis_max)
+
+	return Point{radius * math.Cos(angle), radius * math.Sin(angle)}
+}
+
 func generateBorders(n_points int, size_rough_min, size_rough_max float64) {
 	angle_step := degToRad(360. / float64(n_points))
-	angle_variation_min := -angle_step / 2
-	angle_variation_max := angle_step / 2
+	angle_variation := angle_step / 2
+
+	points := make([]Point, n_points)
 
 	for i := 0; i < n_points; i++ {
-		angle := angle_step*float64(i) + randFloat(angle_variation_min, angle_variation_max)
-		radius := randFloat(size_rough_min/2, size_rough_max/2)
+		angle := angle_step * float64(i)
+		points[i] = generateRadialRandomPoint(angle-angle_variation, angle+angle_variation, size_rough_min/2, size_rough_max/2)
 
-		x := radius * math.Cos(angle)
-		y := radius * math.Sin(angle)
-		fmt.Printf("%6.2f\t%6.1f\t%6.1f\t%6.1f\n", angle, radius, x, y)
-		//fmt.Printf("%7.1f, %7.1f\n", x, y)
+		fmt.Printf("%7.1f\t%7.1f\n", points[i].X, points[i].Y)
 	}
 }
 
