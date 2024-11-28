79caac135446f7a0d513fc2c9466240e2165a1d5
Sergey-K-Chernov <sergey.k.chernov@yandex.ru>
2024-11-28 10:00:14 -0500

    Load roads page and read roads parameters. Prepare for roads generation

---
+++ web/borders.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"html/template"
+	"strconv"
+	gen "chirrwick.com/projects/city/generator"
+	"chirrwick.com/projects/city/city_map"
+	"encoding/json"
+)
+
+type BordersResponse struct{
+	Error string
+	Map string
+	Image string
+	MinR float64
+	MaxR float64
+	NCorners int
+	Variation float64
+}
+
+func readBorderParams(r *http.Request) (bool, BordersResponse, gen.InitialValuesMap) {
+	resp := BordersResponse{Error: "", Map: "{}"}
+	var initials gen.InitialValuesMap
+
+	min_r, err := strconv.ParseFloat(r.FormValue("min_r"), 32)
+        if err != nil {
+		resp.Error = "Cannot read min radius"
+		return false, resp, initials
+	}
+	resp.MinR = min_r
+
+        max_r, err := strconv.ParseFloat(r.FormValue("max_r"), 32)
+        if err != nil {
+		resp.Error = "Cannot read max radius"
+                return false, resp, initials
+	}
+	resp.MaxR = max_r
+
+	n_corners, err := strconv.ParseInt(r.FormValue("n_corners"), 10, 32)
+        if err != nil {
+                resp.Error = "Cannot read corners"
+                return false, resp, initials
+        }
+	resp.NCorners = int(n_corners)
+
+	vartn, err := strconv.ParseFloat(r.FormValue("variation"), 32)
+	if err != nil {
+                resp.Error = "Cannot read variation"
+                return false, resp, initials
+        }
+	resp.Variation = vartn
+
+	initials.Raduis.Min = min_r
+	initials.Raduis.Max = max_r
+	initials.NumSides = int(n_corners)
+	initials.VertexShift = vartn
+
+	return true, resp, initials
+}
+
+func generateBorders(initial_values gen.InitialValuesMap) city_map.Map {
+        channel := make(chan city_map.Map)
+
+        go gen.GenerateBorders(channel, initial_values)
+
+        city_map := <- channel
+
+        return city_map
+}
+
+
+func bordersHandler(w http.ResponseWriter, r *http.Request){
+	fmt.Printf("Serving %s for %s", r.Host, r.URL.Path)
+	index_template := template.Must(template.ParseGlob("./html/borders.gohtml"))
+
+	success, response, initial_values := readBorderParams(r)
+
+	if !success {
+		index_template.ExecuteTemplate(w, "borders.gohtml", response)
+		return
+	}
+
+	fmt.Println()
+	fmt.Println("Borders read successfully? -", success)
+	fmt.Println(initial_values)
+
+	city_map := generateBorders(initial_values)
+
+	map_json, err := json.Marshal(city_map)
+	if err != nil {
+                response.Error = "Error while generating map"
+		response.Map = "{}"
+        }
+	fmt.Println(map_json)
+	response.Map = string(map_json)
+
+	img, success := makeImageString(city_map)
+	if success {
+		response.Image = img
+	}
+
+	index_template.ExecuteTemplate(w, "borders.gohtml", response)
+}
---
+++ web/roads.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"html/template"
+	"strconv"
+	gen "chirrwick.com/projects/city/generator"
+	"chirrwick.com/projects/city/city_map"
+	"encoding/json"
+)
+
+type RoadsResponse struct{
+	Default bool
+	Error string
+	Map string
+	Image string
+	MinR float64
+	MaxR float64
+	NCenters int
+	Branching int
+}
+
+func readRoadsParams(r *http.Request) (bool, RoadsResponse, gen.InitialValuesRoads, city_map.Map) {
+	resp := RoadsResponse{Error: "", Map: "{}"}
+	resp.Default = false
+
+	var initials gen.InitialValuesRoads
+
+	map_string := r.FormValue("map")
+	resp.Map = map_string
+	map_json := []byte(map_string)
+	var city_map city_map.Map
+	err := json.Unmarshal(map_json, &city_map)
+	if err != nil {
+		resp.Error = "Cannot get map from you"
+		return false, resp, initials, city_map
+	}
+	resp.Default = true
+
+	min_r, err := strconv.ParseFloat(r.FormValue("min_r"), 32)
+        if err != nil {
+		resp.Error = "Cannot read min radius"
+		return false, resp, initials, city_map
+	}
+	resp.MinR = min_r
+
+        max_r, err := strconv.ParseFloat(r.FormValue("max_r"), 32)
+        if err != nil {
+		resp.Error = "Cannot read max radius"
+                return false, resp, initials, city_map
+	}
+	resp.MaxR = max_r
+
+	n_centers, err := strconv.ParseInt(r.FormValue("n_centers"), 10, 32)
+        if err != nil {
+                resp.Error = "Cannot read centers"
+                return false, resp, initials, city_map
+        }
+	resp.NCenters = int(n_centers)
+
+	branching, err := strconv.ParseInt(r.FormValue("branching"), 10, 32)
+	if err != nil {
+                resp.Error = "Cannot read road exits"
+                return false, resp, initials, city_map
+        }
+	resp.Branching = int(branching)
+
+	initials.Raduis.Min = min_r
+	initials.Raduis.Max = max_r
+	initials.NumCenters = int(n_centers)
+	initials.Branching = int(branching)
+
+	resp.Default = false
+	return true, resp, initials, city_map
+}
+
+func generateRoads(initial_values gen.InitialValuesRoads, cm city_map.Map) city_map.Map {
+	channel := make(chan city_map.Map)
+
+	go gen.GenerateRoads(cm, channel, initial_values)
+
+	cm = <- channel
+
+	return cm
+}
+
+
+func roadsHandler(w http.ResponseWriter, r *http.Request){
+	fmt.Printf("Serving %s for %s", r.Host, r.URL.Path)
+	index_template := template.Must(template.ParseGlob("./html/roads.gohtml"))
+
+	success, response, initial_values, city_map := readRoadsParams(r)
+
+	if !success {
+		img, success := makeImageString(city_map)
+		if success {
+		    response.Image = img
+		}
+		index_template.ExecuteTemplate(w, "roads.gohtml", response)
+		
+		return
+	}
+
+	fmt.Println()
+	fmt.Println("Roads read successfully? -", success)
+	fmt.Println(initial_values)
+
+	city_map = generateRoads(initial_values, city_map)
+
+	map_json, err := json.Marshal(city_map)
+	if err != nil {
+                response.Error = "Error while generating roads"
+		response.Map = "{}"
+        }
+	fmt.Println(map_json)
+	response.Map = string(map_json)
+
+	img, success := makeImageString(city_map)
+	if success {
+		response.Image = img
+	}
+
+	index_template.ExecuteTemplate(w, "roads.gohtml", response)
+}
